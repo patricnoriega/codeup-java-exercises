78b18933d0dd68711dcf6c9dcb141a4da044e350
patric noriega <patricnoriega@gmail.com>
2021-10-22 11:08:16 -0500

    exercise 2 complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,84 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber + " is my favorite number");
+
+        String myString = "This is mystring talking";
+        System.out.println(myString);
+
+//      String myString = 3.14159;
+//      System.out.println(myString);
+
+//      long myNumber = ;
+//      System.out.print(myNumber);
+
+//      long myNumber = 3.14;
+//      System.out.print(myNumber);
+
+//      long myNumber = 123L;
+//      System.out.print(myNumber);
+
+        long myNumber = 123;
+        System.out.println(myNumber);
+
+//        double myNumber = 3.14;
+//        System.out.print(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+//        ----long hand:----
+//        1) int x = 4;
+//           x = x + 5;
+//           System.out.print(x);
+
+//       2) int x = 3;
+//          int y = 4;
+//          y = y * x;
+//
+//       3) int x = 10;
+//          int y = 2;
+//          x = x / y;
+//          y = y - x;
+
+//        ----vs. short hand:----
+//        int x = 4;
+//         x += 5;
+//        System.out.print(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.print(y);
+
+        int x = 10;
+        int y = 2;
+         x /= y;
+         y -= x;
+         System.out.println(x);
+         System.out.println(y);
+
+
+
+
     }
+
+
+
+
+
+
+
 }
+
