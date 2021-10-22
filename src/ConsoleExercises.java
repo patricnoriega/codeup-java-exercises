80bdcc46c2b579e0ac10b85cc3cf5400e8e1b9b2
patric noriega <patricnoriega@gmail.com>
2021-10-22 16:34:16 -0500

    feat: exercise refactor

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -9,34 +9,35 @@ import java.util.Scanner;
 //        Prompt a user to enter a integer and store that value in an int variable using the nextInt method
         Scanner scanner = new Scanner(System.in);
 //
-//        System.out.print("Enter something: ");
-//        int userInput = scanner.nextInt();
-//
-//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+        System.out.print("Enter a Integer: ");
+        int userInput = scanner.nextInt();
+
+        System.out.println("You entered: \"" + userInput + "\" ");
 
 //        Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline
 
-//        System.out.print("Enter three words: ");
-//        String firstWord = scanner.next();
-//        String secondWord = scanner.next();
-//        String thirdWord = scanner.next();
-//
-//        System.out.println("\"" + firstWord + "\n" +  secondWord + "\n" + thirdWord + "\" ");
+        System.out.print("Enter three words: ");
+        String firstWord = scanner.next();
+        String secondWord = scanner.next();
+        String thirdWord = scanner.next();
+
+        System.out.println("\"" + firstWord + "\n" +  secondWord + "\n" + thirdWord + "\" ");
 
 //        Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
 //
-//        System.out.print("Enter a sentence: ");
-//        String userSentence = scanner.nextLine();
-//        System.out.print(userSentence);
+        System.out.print("Enter a sentence: ");
+        scanner.nextLine();
+        String userSentence = scanner.nextLine();
+        System.out.println(userSentence);
 
 //        Prompt the user to enter values of length and width of a classroom at Codeup.
 //        Use the nextLine method to get user input and parse the resulting string to a numeric type.
 //        -Assume that the rooms are perfect rectangles.
 //        -Assume that the user will enter valid numeric data for length and width.
-            System.out.println("Enter length of your room to find the Perimeter: ");
+            System.out.print("Enter length of your room to find the Perimeter: ");
             double perimeterLength = scanner.nextDouble();
 
-            System.out.println("Enter width of your room to find the Perimeter: ");
+            System.out.print("Enter width of your room to find the Perimeter: ");
             double perimeterWidth = scanner.nextDouble();
 
             double  perimeter = 2*(perimeterLength+perimeterWidth);
@@ -44,10 +45,10 @@ import java.util.Scanner;
             System.out.println("perimeter of the room is: " + perimeter);
 
 
-            System.out.println("Enter the length of the room to find the Area: ");
+            System.out.print("Enter the length of the room to find the Area: ");
             double areaLength = scanner.nextDouble();
 
-            System.out.println("Enter the width of the room to find the Area: ");
+            System.out.print("Enter the width of the room to find the Area: ");
             double areaWidth = scanner.nextDouble();
 
             //Area = length*width;
