7fae276eda76648be7da5088b4e9823bb238faba
patric noriega <patricnoriega@gmail.com>
2021-10-26 15:33:09 -0500

    feat: passing methods to main

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class MethodsExercises {
+import java.util.Scanner;
+
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+        System.out.println(addition(5,10));
+        System.out.println(subtraction(999,1));
+        System.out.println(multiplication(10,10));
+        System.out.println(division(80,4));
+
+    }
+
+
+
+
+
+
+
+
+    public static int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+    public static int division(int num1, int num2) {
+        return num1 / num2;
+    }
 }
