cf2d5c30b322e9752eb0c34e52927fa10d5d1f57
patric noriega <patricnoriega@gmail.com>
2021-11-01 16:26:20 -0500

    feat: update in movies exercise still working through

---
+++ src/movies/movie.java
@@ -0,0 +1,20 @@
+package movies;
+
+public class movie {
+
+    private String name;
+    private String category;
+
+    public String getMovie (String name){
+        return name;
+    }
+
+    public String getCategory (String category){
+    return category;
+
+    }
+
+    public String movie(String name, String category) {
+
+    }
+}
