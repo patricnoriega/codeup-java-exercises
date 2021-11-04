cdbda511a23834e818665722203d5240514ec477
patric noriega <patricnoriega@gmail.com>
2021-11-04 14:29:08 -0500

    feat: abstract/interface shapes runs

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,39 @@
 package shapes;
 
-public class Rectangle {
-    protected int length;
-    protected int width;
-
+public class Rectangle extends Quadrilateral implements Measurable{
     public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
+        super(length, width);
     }
 
-    public int getArea() {
+
+    public double getPerimeter() {
+        return 2 * length + 2 * width;
+    }
+
+    public double getArea() {
         return length * width;
     }
 
-    public int getPerimeter() {
-        return 2 * length + 2 * width;
+    int setLength() {
+        return length;
     }
+
+    int setWidth() {
+        return width;
+    }
+//    protected int length;
+//    protected int width;
+
+//    public Rectangle(int length, int width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public int getArea() {
+//        return length * width;
+//    }
+//
+//    public int getPerimeter() {
+//        return 2 * length + 2 * width;
+//    }
 }
---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,34 @@
+package shapes;
+
+import shapes.Rectangle;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+//        Rectangle box1 = new Rectangle(4,5);
+//        System.out.println("Rectangle box1 instance of Rectangle Perimeter = " + box1.getPerimeter() );
+//        System.out.println("Rectangle box1 instance of Rectangle Area = " + box1.getArea() );
+//
+//        System.out.println();
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println("Rectangle box2 instance of shapes.Square Perimeter = " + box2.getPerimeter() );
+//        System.out.println("Rectangle box2 instance of shapes.Square Area = " + box2.getArea() );
+
+        Measurable myShape = new Rectangle(4,5);
+
+        System.out.println("Rectangle box1 perimeter = " + myShape.getPerimeter() );
+        System.out.println("Rectangle box1 area = " + myShape.getArea() );
+
+        System.out.println();
+
+        Measurable myShape2 = new Square(3,3);
+        System.out.println("Square box2 perimeter = " + myShape2.getPerimeter() );
+        System.out.println("Square box2 perimeter = " + myShape2.getArea() );
+
+
+
+
+
+
+    }
+}
